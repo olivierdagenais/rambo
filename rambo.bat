2f730f03d927ee2e31790e581b48efcc3f663ab7
Olivier Dagenais <olivier.dagenais@gmail.com>
2021-04-10 16:57:43 -0400

    fix: support both 32-bit and 64-bit launches

--- rambo.bat
+++ rambo.bat
@@ -37,7 +37,13 @@ echo ^</Configuration^> >> rambo.wsb
 echo @echo off > rambo_launch.bat
 echo start "rambo_launch" /D "%~dp1 " "%~f1" >> rambo_launch.bat
 
-"%windir%\Sysnative\WindowsSandbox.exe" rambo.wsb
+if EXIST "%windir%\Sysnative" (
+    SET BASEFOLDER=%windir%\Sysnative
+) else (
+    SET BASEFOLDER=%windir%\System32
+)
+
+"%BASEFOLDER%\WindowsSandbox.exe" rambo.wsb
 
 del rambo.wsb
 del rambo_launch.bat
